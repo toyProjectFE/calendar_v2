933b408a4e7126acc41f4483dcbc32cdf00ceb61
jinhwansong <119153742+jinhwansong@users.noreply.github.com>
2023-02-25 01:51:17 +0900

    라우터 변경

--- src/router/Router.js
+++ src/router/Router.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Detail from '../share/page/Detail'
 const Router =()=>{
-    <BrowserRouter>
-        <Routes>
-            {/* <Route path="/" element={<login/>}/>
-            <Route path="/main" element={<main/>}/> */}
-            <Route path="/Detail" element={<Detail/>}/>
-        </Routes>
-    </BrowserRouter>
+    return(
+        <BrowserRouter>
+            <Routes>
+                {/* <Route path="/" element={<login/>}/>
+                <Route path="/main" element={<main/>}/> */}
+                <Route path="/Detail" element={<Detail/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default Router
