34070501ac5b9f338df24c4173667c0051470e5c
GIBeom-Cheon <110073263+GIBeom-Cheon@users.noreply.github.com>
2023-02-25 20:55:57 +0900

    쪼금 수정

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,26 +9,23 @@ import {
   LoginBtnKaKAo,
   DivContainer,
   FormContainer,
-  Loginbox,
 } from "./styles";
 
 function Login() {
   return (
     <MainContainer>
       <StyledDiv>
-        <Loginbox>
-          <LoginTitle>Login</LoginTitle>
-          <FormContainer>
-            <IdInput placeholder="아이디를 적어주세요" />
-            <IdInput type="password" placeholder="비밀번호를 적어주세요" />
-            <LoginBtn>Login</LoginBtn>
-            <LoginBtnKaKAo>kakao Login</LoginBtnKaKAo>
-            <DivContainer>
-              <span>아직 회원 아님?</span>
-              <span>회원가입</span>
-            </DivContainer>
-          </FormContainer>
-        </Loginbox>
+        <LoginTitle>Login</LoginTitle>
+        <FormContainer>
+          <IdInput placeholder="아이디를 적어주세요" />
+          <IdInput type="password" placeholder="비밀번호를 적어주세요" />
+          <LoginBtn>Login</LoginBtn>
+          <LoginBtnKaKAo>kakao Login</LoginBtnKaKAo>
+          <DivContainer>
+            <span>아직 회원 아님?</span>
+            <span>회원가입</span>
+          </DivContainer>
+        </FormContainer>
       </StyledDiv>
       <Liha>동영상이지롱</Liha>
     </MainContainer>
--- src/page/Detail.jsx
+++ src/page/Detail.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Detailleftbox from "../components/detail/Detailleftbox";
 import Detiallrightbox from "../components/detail/Detiallrightbox";
 import { Detailbox } from "../components/detail/style";
+
 function Detail() {
   return (
     <>
-      {/* <RenderHeader /> */}
       <Detailbox>
         <Detailleftbox />
         <Detiallrightbox />
