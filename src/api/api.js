be80b81da61232fbef6ddc76b4f542b31f9764da
jinhwansong <119153742+jinhwansong@users.noreply.github.com>
2023-02-27 23:04:36 +0900

    상세페이지 수정

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,25 @@
 import axios from "axios"
-//조회
-const getCalender = async()=>{
-    const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/user`)
-    return response.data
-}
-//생성
-const addCalender = async(newCalender)=>{
-    await axios.get(`${process.env.REACT_APP_SERVER_URL}/user`,newCalender)
-}
-export {getCalender,addCalender}
+
+
+
+//조회 디테일
+const getDetail = async () => {
+    const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/schedule`);
+    return response.data;
+};
+
+
+//생성 디테일
+const addSchedule = async (newSchedule) => {
+await axios.post(`${process.env.REACT_APP_SERVER_URL}/schedule`, newSchedule);
+};
+
+//트루펄스 디테일
+const swichSchedule = async (payload) => {
+    await axios.patch(`${process.env.REACT_APP_SERVER_URL}/schedule/${payload.id}`,{
+        complete:payload.complete
+    });
+};
+
+
+export { swichSchedule,getDetail,addSchedule };
---
+++ src/components/detail/Card.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { Content, Title, Button, Bottom, Allboxli, Modalbg } from "./style";
+function Card({ state, swichhander }) {
+  const [ModalOpen, setModalOpen] = useState(false);
+  const ModalButton = (id) => {
+    setModalOpen(id);
+  };
+  return (
+    <Allboxli key={state.id}>
+      <Button onClick={() => swichhander(state.id)}>
+        <svg
+          width="12"
+          height="9"
+          viewBox="0 0 12 9"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="4.24265"
+            y="8.48528"
+            width="6"
+            height="2"
+            transform="rotate(-135 4.24265 8.48528)"
+            fill="white"
+          />
+          <rect
+            x="2.82843"
+            y="7.07108"
+            width="10"
+            height="2"
+            transform="rotate(-45 2.82843 7.07108)"
+            fill="white"
+          />
+        </svg>
+      </Button>
+      <div onClick={() => ModalButton(state.id)}>
+        <p>{state.author}</p>
+        <Bottom>
+          <Title>{state.title}</Title>
+          <Content>{state.contents}</Content>
+        </Bottom>
+      </div>
+      {ModalOpen === state.id && (
+        <Modalbg setModalopen={setModalOpen} state={state}></Modalbg>
+      )}
+    </Allboxli>
+  );
+}
+
+export default Card
